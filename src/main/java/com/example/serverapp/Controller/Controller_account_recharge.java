2a2f9470dc4aee6ada9cae49fd8b1f2e8de6eeae
Aro <99348310+AroFifa@users.noreply.github.com>
2023-02-03 17:17:02 +0300

    teste de modification

--- src/main/java/com/example/serverapp/Controller/Controller_account_recharge.java
+++ src/main/java/com/example/serverapp/Controller/Controller_account_recharge.java
@@ -80,22 +80,24 @@ public class Controller_account_recharge {
 
     @PostMapping("{recharge_id}/valid")
     public Object validate( @PathVariable int recharge_id,@RequestBody Account_recharge_validation account_recharge_validation){
-        try{
-//            Tokenadmin t=new Tokenadmin().check_Expiration(token,getRepo_tokenadmin());
-//            if(t==null)
-//                return new ResponseError("Access denied");
+//        try{
+////            Tokenadmin t=new Tokenadmin().check_Expiration(token,getRepo_tokenadmin());
+////            if(t==null)
+////                return new ResponseError("Access denied");
+//
+//            Account_recharge r=new Account_recharge();
+//            r.setId(recharge_id);
+//            account_recharge_validation.setAccount_recharge(r);
+//
+//            getRepo_recharge_validation().save(account_recharge_validation);
+//        }catch (Throwable e){
+//
+//            e.printStackTrace();
+//            return new ResponseError(e.getMessage());
+//        }
+//        return new ResponseData("Success");
 
-            Account_recharge r=new Account_recharge();
-            r.setId(recharge_id);
-            account_recharge_validation.setAccount_recharge(r);
-
-            getRepo_recharge_validation().save(account_recharge_validation);
-        }catch (Throwable e){
-
-            e.printStackTrace();
-            return new ResponseError(e.getMessage());
-        }
-        return new ResponseData("Success");
+        return new ResponseData("success");
     }
 
     @GetMapping()
